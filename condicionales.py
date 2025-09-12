073d82398cac00fb7de3536b7e6859edc8a634be
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-09-12 15:39:30 -0600

    ejercicios codigo

--- condicionales.py
+++ condicionales.py
@@ -1,5 +1,5 @@
 # Condicionales en Python, ejercicios y explicacion
-
+"""
 x = 5
 
 if x > 0:
@@ -8,6 +8,9 @@ elif x > 10:
     print("x es mayor que 10")
 else:
     print("x es menor o igual a 0")
+
+"""
+
 
 #El ejemplo anterior tiene una "trampa" para revisar la logica 
 

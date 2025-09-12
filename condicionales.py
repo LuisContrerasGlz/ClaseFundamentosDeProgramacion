da850ec532155bca31c3429ee9bf82b6255922cd
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-09-12 14:40:33 -0600

    ejemplos condicionales

--- condicionales.py
+++ condicionales.py
@@ -1,1 +1,14 @@
 # Condicionales en Python, ejercicios y explicacion
+
+x = 5
+
+if x > 0:
+    print("x es mayor que 0")
+elif x > 10:
+    print("x es mayor que 10")
+else:
+    print("x es menor o igual a 0")
+
+#El ejemplo anterior tiene una "trampa" para revisar la logica 
+
+

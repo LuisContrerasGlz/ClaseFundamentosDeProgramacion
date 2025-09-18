6cca83f1b6390e5f2a6b234f16548809074e14c5
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-09-17 19:32:34 -0600

    resultado 3

--- if_else_ejercicios.py
+++ if_else_ejercicios.py
@@ -32,15 +32,18 @@ else:
 
 
 """
-
- 
-
 3. Par o impar
 
 Solicita un número y muestra si es par o impar.
 
 """
 
+numero = int(input("Escribe un número: "))
+
+if numero % 2 == 0:
+    print(f"El número {numero} es par")
+else:
+    print(f"El número {numero} es impar")
 
 
 """

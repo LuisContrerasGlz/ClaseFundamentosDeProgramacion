200ba4e97d058ca5c79af0e321884099d70a9792
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-09-17 19:37:43 -0600

    resultado 9

--- if_else_ejercicios.py
+++ if_else_ejercicios.py
@@ -160,6 +160,12 @@ Pide una letra y determina si es una vocal o consonante.
 
 """
 
+letra = input("Escribe una letra: ").lower()
+
+if letra in "aeiou":
+    print(f"La letra {letra} es una vocal")
+else:
+    print(f"La letra {letra} es una consonante")
 
 
 """

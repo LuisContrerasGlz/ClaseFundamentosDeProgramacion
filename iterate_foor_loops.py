a9349540fae90297796d5171a0675891704bccbf
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-10-08 15:18:33 -0600

    for loops iteration

---
+++ iterate_foor_loops.py
@@ -0,0 +1,19 @@
+# Iteracion "clasica" sobre una lista
+lst = [1, 2, 3, 4, 5, True, False]
+
+for i in range(7):
+    print(lst[i])
+
+
+# Items de mas
+lst = [1, 2, 3, 4, 5, True, False,9]
+
+for i in range(7):
+    print(lst[i])
+
+# Items de menos
+lst = [1, 2, 3, 4, 5]
+
+for i in range(7):
+    print(lst[i])
+

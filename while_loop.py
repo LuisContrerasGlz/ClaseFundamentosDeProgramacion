b47fd1a6daa71b48871aa6eb50290e6f4a1bc814
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-10-10 00:42:54 -0600

    while loop

--- while_loop.py
+++ while_loop.py
@@ -14,4 +14,46 @@ while x < 5:
 for x in range(5):
     print(x)
 
+# Uso de break
+while True:
+    num = input("Enter an integer: ")
+    if num.isdigit():
+        break
+
+lst = [2, 3, 3, 2, 2, 1]
+
+result = 0
+i = 0
+
+# Mientras la suma acumulada (result) sea menor que 9
+while result < 9:
+    # Tomamos el elemento actual de la lista
+    num = lst[i]      
+    # Lo sumamos al acumulador     
+    result += num   
+    # Pasamos al siguiente índice       
+    i += 1   
+    # Imprimimos              
+    print(num)
+
+# Si no hay numeros que coincidan
+
+lst = [2, 3, 3, -2, -2, -1]
+
+result = 0
+i = 0
+
+# El bucle se ejecuta mientras la suma acumulada (result) sea menor que 9
+# Y mientras no se haya llegado al final de la lista
+while result < 9 and i < len(lst):
+    # Tomamos el elemento actual de la lista
+    num = lst[i]    
+    # Lo sumamos al acumulador      
+    result += num       
+    # Pasamos al siguiente índice  
+    i += 1  
+    # Imprimimos el número procesado en esta iteración             
+    print(num)            
+
+
 

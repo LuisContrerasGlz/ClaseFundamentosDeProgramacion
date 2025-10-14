f2c57513df7cd2e24f9efb51fe381f56ca7a32b0
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-10-14 13:37:10 -0600

    progress in code

---
+++ dic.py
@@ -0,0 +1,41 @@
+ages = {
+    "Luis": 30,
+    "Alex": 33,
+    "Juan": 30
+}
+
+print("Alex tiene", ages["Alex"], "años")
+
+# Acceder a los elementos del diccionario
+
+x = {2: "hello", "1": 5}
+
+print(x[2])
+
+print(x["1"])
+
+# Agregar un elemento a un diccionario vacío
+
+x = {}
+
+x["key"] = "value"
+
+print(x)
+print(x["key"])
+
+# Modificar el valor de una clave existente
+
+x = {"key": 1}
+
+x["key"] = "value"
+
+print(x["key"])
+
+# Agregar una nueva clave al diccionario
+
+x = {"key": 1}
+
+x["key2"] = "value"
+
+print(x)
+

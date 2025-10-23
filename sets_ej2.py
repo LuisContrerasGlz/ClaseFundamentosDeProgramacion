38bde89d667e1fce95c3c03de0b53542e8299bfc
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-10-23 14:25:17 -0600

    sets ex

--- sets_ej2.py
+++ sets_ej2.py
@@ -22,3 +22,23 @@ numeros = [1, 2, 2, 3, 3, 3, 4]
 sin_duplicados = list(set(numeros))
 
 print("Lista sin duplicados:", sin_duplicados)
+
+# Detectar el primer elemento repetido en una lista
+
+numeros = [4, 2, 5, 1, 2, 4, 7, 5]
+
+vistos = set()
+duplicado = None
+
+for n in numeros:
+    if n in vistos:
+        duplicado = n
+        break
+    vistos.add(n)
+
+if duplicado:
+    print("El primer duplicado es:", duplicado)
+else:
+    print("No hay elementos duplicados.")
+
+

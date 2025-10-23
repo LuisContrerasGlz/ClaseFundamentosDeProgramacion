c26424e57e58f9f5242977ac493396f202fb07d4
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-10-23 14:23:46 -0600

    sets ex

--- sets_ej2.py
+++ sets_ej2.py
@@ -15,3 +15,10 @@ lista2 = [3, 4, 5, 6, 7]
 comunes = set(lista1) & set(lista2)
 
 print("Elementos comunes:", comunes)
+
+# Eliminar duplicados de una lista sin usar bucles
+
+numeros = [1, 2, 2, 3, 3, 3, 4]
+sin_duplicados = list(set(numeros))
+
+print("Lista sin duplicados:", sin_duplicados)

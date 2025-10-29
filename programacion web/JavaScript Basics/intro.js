1378410c040f16122c2a610ebc20d91446701243
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-10-29 17:35:27 -0600

    array

--- programacion web/JavaScript Basics/intro.js
+++ programacion web/JavaScript Basics/intro.js
@@ -7,3 +7,11 @@ const numeros = [10, 20, 30, 40];
 for (let i = 0; i < numeros.length; i++) {
   console.log("Índice:", i, "→ Valor:", numeros[i]);
 }
+
+// Usando For of moderno
+
+const numeros2 = [10, 20, 30, 40];
+
+for (let n of numeros2) {
+  console.log("Valor:", n);
+}

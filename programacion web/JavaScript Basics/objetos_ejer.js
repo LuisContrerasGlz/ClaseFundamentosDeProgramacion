c912afb900789568659ec0f49b2bacfc89653b31
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-11-03 16:47:12 -0600

    objetos en js

--- programacion web/JavaScript Basics/objetos_ejer.js
+++ programacion web/JavaScript Basics/objetos_ejer.js
@@ -7,3 +7,19 @@ const persona = {
 };
 
 console.log(`${persona.nombre} tiene ${persona.edad} años y es ${persona.ocupacion}.`);
+
+/*
+
+Usando el objeto persona del ejercicio anterior:
+
+Agrega una nueva propiedad llamada ciudad.
+
+Cambia la propiedad ocupacion a otro valor.
+
+Muestra el objeto actualizado.
+
+*/
+
+persona.ciudad = "Guadalajara";
+persona.ocupacion = "Diseñadora";
+console.log(persona);

45b61089fc85fe0ac517dbc212d4d29f30aff783
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-11-03 16:45:56 -0600

    objetos en js

---
+++ programacion web/JavaScript Basics/objetos_ejer.js
@@ -0,0 +1,9 @@
+// Crea un objeto llamado persona con las propiedades: nombre, edad, y ocupacion y Muestra en consola una frase con esa información.
+
+const persona = {
+  nombre: "Ana",
+  edad: 25,
+  ocupacion: "Ingeniera"
+};
+
+console.log(`${persona.nombre} tiene ${persona.edad} años y es ${persona.ocupacion}.`);

030c68901f68ebb53eda1a87c79001dc3112e6b1
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-11-06 09:52:17 -0600

    ejemplos objetos js

--- programacion web/JavaScript Basics/objetos_explic.js
+++ programacion web/JavaScript Basics/objetos_explic.js
@@ -18,5 +18,21 @@ const website2 = {
 
 console.log(website2.name);
 console.log(website2['name']);
+// Podemos usar [] cuando la key tenga espacios o caracteres entre ellos
+console.log(website2['name for example test']);
+console.log(website2['name_example_test']);
+// Si queremos acceder a algo que no existe nos dara undefined 
+
+// Aun que declaramos el objeto como const podemos modificar el contenido ya que no lo estamos redeclarando
+
+website.name = 'FrontendExpert';
+console.log(website.name);
+
+website.foo = 'bar';
+console.log(website);
+
+delete website.foo;
+console.log(website);
+
 
 

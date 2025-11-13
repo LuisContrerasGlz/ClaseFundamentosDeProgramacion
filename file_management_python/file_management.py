5a50ff6b24454a4aa0bd1bd6250f5a275a963ab9
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-11-13 15:04:48 -0600

    File management python

--- file_management_python/file_management.py
+++ file_management_python/file_management.py
@@ -1,3 +1,9 @@
+# W mode for write
 write_file = open(r'FakeFile.txt', 'w')
 write_file.write('This is our first sentence in our file.')
 write_file.close()
+
+# A for us to append to the file
+append_file = open(r'FakeFile.txt', 'a')
+append_file.write(' This is our Second sentence in our file.')
+append_file.close()

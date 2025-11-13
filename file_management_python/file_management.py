15cd8f68f27a70396a7d6f9482ff0d3e5759735b
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-11-13 15:10:04 -0600

    File management python

--- file_management_python/file_management.py
+++ file_management_python/file_management.py
@@ -7,3 +7,14 @@ write_file.close()
 append_file = open(r'FakeFile.txt', 'a')
 append_file.write(' This is our Second sentence in our file.')
 append_file.close()
+
+# Write multi line
+
+multi_line = """
+This is the Fourth sentence.
+This is the Fifth sentence.
+This is the Sixth sentence.
+"""
+
+with open(r'FakeFile.txt', 'a') as append_file:
+    append_file.write(multi_line)

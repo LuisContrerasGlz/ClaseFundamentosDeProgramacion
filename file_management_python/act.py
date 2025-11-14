afdb10c815cbdcbe3105ae2f62a79e87e499e597
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2025-11-14 15:47:02 -0600

    Act manejo de archivos

---
+++ file_management_python/act.py
@@ -0,0 +1,12 @@
+"""
+El usuario proporciona una ruta a un archivo .txt.
+
+Tu programa debe:
+1. Abrir el archivo (manejar errores).
+2. Contar:
+    * Número de líneas
+    * Número de palabras
+    * Número de caracteres
+3. Guardar el reporte en un archivo llamado "reporte.txt"
+4. Mostrar el reporte en pantalla.
+"""
